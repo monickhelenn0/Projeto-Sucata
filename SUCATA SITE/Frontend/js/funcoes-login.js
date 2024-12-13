6af86f436964642ce2151849675daa5358f6766e
monickhelenn0 <125844893+monickhelenn0@users.noreply.github.com>
2024-12-13 13:49:40 +0000

    adicionando verificação de login as páginas

--- SUCATA SITE/Frontend/js/funcoes-login.js
+++ SUCATA SITE/Frontend/js/funcoes-login.js
@@ -102,3 +102,14 @@ function sair() {
     localStorage.removeItem("usuarioLogado");
     window.location.href = "login.html";
 }
+/**
+ * Verifica se o usuário está logado. Caso contrário, redireciona para a tela de login.
+ */
+(function verificarUsuarioLogado() {
+    const usuarioLogado = localStorage.getItem("usuarioLogado");
+
+    if (!usuarioLogado) {
+        alert("Você precisa estar logado para acessar esta página.");
+        window.location.href = "login.html";
+    }
+})();
