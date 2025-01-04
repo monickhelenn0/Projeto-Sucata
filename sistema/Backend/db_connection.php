de963abdaba681740ffd944bd8eb6f3ecba830bc
monickhelenn0 <125844893+monickhelenn0@users.noreply.github.com>
2025-01-04 12:57:33 +0000

    DB no Render

--- sistema/Backend/db_connection.php
+++ sistema/Backend/db_connection.php
@@ -1,14 +1,17 @@
 <?php
-$host = 'sql308.infinityfree.com'; // Substitua pelo hostname correto
-$user = 'if0_37921675';            // Seu nome de usuário no InfinityFree
-$password = 'Sucata22';            // Sua senha no InfinityFree
-$database = 'if0_37921675_database'; // Nome do banco de dados
+$host = 'dpg-ctsiv65umphs73ifn0a10-a'; // Hostname do Render
+$port = '5432';                        // Porta fornecida
+$user = 'sucata';                      // Usuário
+$password = 'x00onMFxEEqkWli46XMTtnx5tWliDMvy'; // Senha fornecida
+$database = 'sucata';                  // Nome do banco de dados
 
 // Estabelecer conexão
-$connection = new mysqli($host, $user, $password, $database);
+$connection = new mysqli($host, $user, $password, $database, $port);
 
 // Verificar conexão
 if ($connection->connect_error) {
-    die("Erro na conexão: " . $connection->connect_error);
+    die("Erro ao conectar ao banco de dados: " . $connection->connect_error);
 }
+
+echo "Conexão bem-sucedida com o banco de dados!";
 ?>
