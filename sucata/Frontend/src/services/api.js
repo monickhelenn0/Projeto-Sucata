e7a3894e0130d57be77bc2bb0b9ec254e05eb640
monickhelenn0 <125844893+monickhelenn0@users.noreply.github.com>
2025-01-04 12:03:59 +0000

    retirada de sucata, permanece SUCATA SITE

--- sucata/Frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// URL base do backend no Render
-const api = axios.create({
-  baseURL: 'https://projeto-sucata.onrender.com', // Substitua pela URL do backend no Render
-});
-
-export default api;
